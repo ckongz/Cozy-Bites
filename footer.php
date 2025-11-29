f216a5380000fb4fdc1c39d4fe6545e56f7ffea1
ckongz <139482669+ckongz@users.noreply.github.com>
2025-11-29 19:38:05 +0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,5 @@
+<!-- Footer -->
+<footer>
+    <p>&copy; 2025 Cozy Bites. All rights reserved.</p>
+    <p>Sophia Ong | WD - 203 </p>
+</footer>
---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Cozy Bites - Home</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <!-- Navigation -->
+    <nav class="navbar">
+        <section class="nav-container">
+            <section class="logo">
+                <img src="img/Logo.png" alt="Cozy Bites Logo" class="logo-img">
+                <span class="logo-text">Cozy Bites</span>
+            </section>
+            <ul class="nav-links">
+                <li><a href="home.php" class="active">Home</a></li>
+                <li><a href="pages.php">Products</a></li>
+            </ul>
+        </section>
+    </nav>
